513a8ee721ddf9c35dec49a53f05b45390a1fac4
PetarCetkovic <pcetkovic007@gmail.com>
2019-04-13 15:22:34 +0200

    delete comments

--- iterators.js
+++ iterators.js
@@ -25,14 +25,6 @@ function returnIterator(arr) {
     return inner;
 }
 
-// Uncomment the lines below to test your work
-// const array2 = ['a', 'b', 'c', 'd'];
-// const myIterator = returnIterator(array2);
-// console.log(myIterator()); // -> should log 'a'
-// console.log(myIterator()); // -> should log 'b'
-// console.log(myIterator()); // -> should log 'c'
-// console.log(myIterator()); // -> should log 'd'
-
 
 
 // CHALLENGE 2
